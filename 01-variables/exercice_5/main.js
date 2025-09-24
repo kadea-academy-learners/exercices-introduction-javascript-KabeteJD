4d3bd4b9d5d6ed823c98a1545f63e02ba5dfe84b
KabeteJD <baswikujeandan@gmail.com>
2025-09-24 15:11:11 +0100

    fix: update a name of the variable

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,5 +1,5 @@
 // Exercice 5
 // Écrivez votre code ici
-let Celsius = 19; 
-let Fahrehreit = (Celsius * 9/5) + 32; 
-console.log("Température en Fahrenheit : ",  fahrenheit);
+let Celsius = 19;
+let fahrenheit = (Celsius * 9/5) + 32;
+console.log("Température en Fahrenheit :", fahrenheit);
