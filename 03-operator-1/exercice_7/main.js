1719aab1093123d2fdd25382275d7d285b0b5259
KabeteJD <baswikujeandan@gmail.com>
2025-09-24 12:40:19 +0100

    feat: Uses of Arithmetic Operators in an Age Calculation Script

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,16 @@
 // Exercice 7
 // Écrivez votre code ici
+
+let ageEnfant = 20;
+let agePère = ageEnfant * 2;
+let ageMère = agePère - 5;
+let ageGrandPère = (ageMère * 2) + (ageEnfant / 2);
+let ageOncle = agePère + 10;
+
+// Affichage des résultats
+
+console.log("Âge de l'enfant :", ageEnfant);
+console.log("Âge du père :", agePère);
+console.log("Âge de la mère :", ageMère);
+console.log("Âge du grand-père :", ageGrandPère);
+console.log("Âge de l'oncle :", ageOncle);
