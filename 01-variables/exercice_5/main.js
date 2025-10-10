4a2954951276ea06f61ca8ecb640acf92d82b2a2
KabeteJD <baswikujeandan@gmail.com>
2025-10-10 14:26:38 +0100

    fix : Addded a function in the exercice

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,5 +1,11 @@
 // Exercice 5
 // Écrivez votre code ici
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
 let celsius = 25;
-let fahrenheit = (celsius * 9 / 5) + 32;
-console.log(celsius + "°C = " + fahrenheit + "°F");
+let fahrenheit = celsiusToFahrenheit(celsius);
+console.log(celsius + "°C équivaut à " + celsiusToFahrenheit(celsius) + "°F");
+
+module.exports = {celsiusToFahrenheit};
